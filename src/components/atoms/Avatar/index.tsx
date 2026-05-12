5a92232e8f199476e54ab8913fe5316b59ee27e6
Henrique Garcia <henriquegarcia.code@gmail.com>
2026-05-12 09:41:52 -0300

    feat: Composição da estrutura de componentes

---
+++ src/components/atoms/Avatar/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles';
+
+// interface IAvatar {}
+
+// const Avatar = ({}: IAvatar) => {
+//   return <S.Avatar>Avatar</S.Avatar>;
+// };
+
+// interface IAvatar {}
+
+const Avatar = () => {
+  return <S.Avatar>Avatar</S.Avatar>;
+};
+
+export default Avatar;
